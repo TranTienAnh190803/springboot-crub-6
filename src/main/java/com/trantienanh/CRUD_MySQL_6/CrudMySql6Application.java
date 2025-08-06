a45f488c43734fb96aa082091eeb66003fa1c82c
Tran Tien Anh <anhtin44@gmail.com>
2025-08-06 10:21:53 +0700

    firstcommit

---
+++ src/main/java/com/trantienanh/CRUD_MySQL_6/CrudMySql6Application.java
@@ -0,0 +1,13 @@
+package com.trantienanh.CRUD_MySQL_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudMySql6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudMySql6Application.class, args);
+	}
+
+}
